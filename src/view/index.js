2f27e2c584e9ab2004b8fd8d4e9b628210643a30
nichoth <nichoth@gmail.com>
2021-02-15 21:15:09 -0800

    more better

--- src/view/index.js
+++ src/view/index.js
@@ -2,5 +2,5 @@ import { render } from 'preact'
 import { html } from 'htm/preact'
 
 module.exports = function eventual () {
-    render(html`<p>Hello earth</p>`, document.getElementById('content'));
+    render(html`<p>Hello from JS</p>`, document.getElementById('content'));
 }
